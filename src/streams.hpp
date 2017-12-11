d504d289ca0aded7ff5625f5a79ed50d0c06dda3
Alexis Reynouard <areynoua@ulb.ac.be>
2017-12-11 13:23:32 +0100

    Abstract Streams

---
+++ src/streams.hpp
@@ -0,0 +1,22 @@
+#ifndef __STREAMS_HPP__
+#define __STREAMS_HPP__
+
+#include <string>
+
+using string = std::string;
+
+class AbstractInputstream {
+public:
+  void open(string infilename);
+  int_least32_t read_next();
+  bool end_of_stream();
+};
+
+class AbstractIutputstream {
+public:
+  void create(string outfilename);
+  void write (int_least32_t elem);
+  void close();
+};
+
+#endif //__STREAMS_HPP__
