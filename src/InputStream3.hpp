feb1e051aa1417829c75567268af9ef2aeb2560f
Alexis Reynouard <areynoua@ulb.ac.be>
2017-12-11 16:05:36 +0100

    InputStream3.hpp

--- src/InputStream3.hpp
+++ src/InputStream3.hpp
@@ -4,7 +4,6 @@
 #include <cstdint> // int_least32_t
 #include <fcntl.h>
 #include <fstream>
-#include <string>
 #include <sys/stat.h>
 #include <sys/types.h>
 
@@ -20,10 +19,12 @@ template <size_t _bufferSize>
 class BufferedInputstream  {
     char _buffer[_bufferSize];
     int _fd = -1;
-    unsigned int _next = _bufferSize;
+    size_t _next = 0;
+    size_t _max = 0;
+    bool _eof = false;
 public:
     BufferedInputstream();
-    void open(string infilename);
+    void open_file(const char*);
     int_least32_t read_next();
     bool end_of_stream();
 private:
@@ -37,34 +38,43 @@ BufferedInputstream<_bufferSize>::BufferedInputstream() {
 }
 
 template <size_t _bufferSize>
-void BufferedInputstream<_bufferSize>::open(const char *pathname) {
+void BufferedInputstream<_bufferSize>::open_file(const char *pathname) {
     _fd = open(pathname, O_RDONLY | O_LARGEFILE);
+    // TODO: throw error if fd < 0
     std::cout << pathname << std::endl;
 }
 
 template <size_t _bufferSize>
 int_least32_t BufferedInputstream<_bufferSize>::read_next() {
-    if (_next == _bufferSize) {
-        fillBuffer();
+    if (_next == _max) {
+        _max = read(_fd, _buffer, _bufferSize);
+        std::cout << _max << " bytes read from file" << std::endl;
+        if (_max == 0) {
+            _eof = true;
+        }
+        // TODO: if max == 0: EOF
+        // TODO: if max < 0: ERROR
+        // TODO: not multiple of 4 bytes
         _next = 0;
     }
-    read(_fd, _buffer
-    return 0;
+    int_least32_t ret = charsToInt32(&(_buffer[_next]));
+    _next += 4;
+    return ret;
 }
 
 template <size_t _bufferSize>
 bool BufferedInputstream<_bufferSize>::end_of_stream() {
-    return true;
+    return _eof;
 }
 
 template <size_t _bufferSize>
 int_least32_t BufferedInputstream<_bufferSize>::charsToInt32 (char* chars) {
     return (static_cast<int_least32_t>(chars[0]) << 24)
-         & (static_cast<int_least32_t>(chars[1]) << 16)
-         & (static_cast<int_least32_t>(chars[2]) << 8)
-         & (static_cast<int_least32_t>(chars[3]));
+        | (static_cast<int_least32_t>(chars[1]) << 16)
+        | (static_cast<int_least32_t>(chars[2]) << 8)
+        | (static_cast<int_least32_t>(chars[3]));
 }
 
 #endif // __IMPL3_H__
 
-// vim: set shiftwidth=4
+// vim: set shiftwidth=4:
