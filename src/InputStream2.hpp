60725bc37088a3ba541f69968b474d112348a469
Alexis Reynouard <areynoua@ulb.ac.be>
2017-12-12 11:48:28 +0100

    consistent implementation of InputStream1 and InputStream2

--- src/InputStream2.hpp
+++ src/InputStream2.hpp
@@ -8,49 +8,56 @@
 
 using namespace std;
 
-class InputStream2: virtual public AbstractInputstream {
+class InputStream2 final : virtual public AbstractInputstream {
 private:
+    static const size_t SIZE = 32 / 8; // 32 bits
     FILE *file_pointer;
-    size_t end;
+    char buffer[SIZE];
+    size_t read_size;
 public:
-    InputStream2():file_pointer(nullptr), end(1){}
-    virtual void open(const char* const) override;
-    virtual int_least32_t read_next() override;
-    virtual bool end_of_stream() override;
-    void read_all();
-
-
+    InputStream2(): file_pointer(nullptr), read_size(0){}
+    void open(const char* const) override;
+    int_least32_t read_next() override;
+    bool end_of_stream() override;
 };
 
 
 void InputStream2::open(const char* filename){
     file_pointer = fopen(filename, "r");
-
-    fseek(file_pointer, 0, SEEK_SET);
-    cout << read_next() << endl;
+    if (file_pointer == nullptr) {
+        throw FileOpenException(errno);
+    }
+    fseek(file_pointer, 0, SEEK_SET); // TODO: is it really needed ?
+    read_next(); // fill the buffer
 }
 
 int_least32_t InputStream2::read_next(){
-    char buffer[4];
-    end = fread(buffer,4,1,file_pointer);
-    if (!end_of_stream()){
-        return charsToInt32(buffer);
+    /* Return the value in the buffer when the function is called
+     * and fill the buffer for the next value.
+     * This allow to handle end_of_stream conveniently. */
+
+    int_least32_t value(0);
+
+    if (read_size == SIZE) {
+        value = charsToInt32(buffer);
+    }
+    else {
+        if (read_size > 0) {
+            error(0, 0, "Last %ld bytes ignored (4 bytes expected)", read_size);
+        }
+        else if (read_size < 0) {
+            throw FileReadException(errno);
+        }
     }
 
-    return NULL;
+    // FIXME: fread does not work this way
+    read_size = fread(buffer, SIZE, 1, file_pointer);
+
+    return value;
 }
 
 bool InputStream2::end_of_stream(){
-    return end == 0;
-}
-
-void InputStream2::read_all(){
-    while (end != 0){
-        int_least32_t value = read_next();
-        if (value != NULL){
-            cout << value <<endl;;
-        }
-    }
+    return read_size == 0;
 }
 
 #endif
