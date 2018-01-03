9f089f5d181105588b34cb6a1940fed5b4ade5bf
Alexis Reynouard <areynoua@ulb.ac.be>
2018-01-03 12:38:53 +0100

    better benchmark-streams.sh

--- src/benchmark-streams.sh
+++ src/benchmark-streams.sh
@@ -8,21 +8,24 @@
 # 256 MiB  67108864 int32
 # 512 MiB 134217728 int32
 
-alias time=/usr/bin/time
-
 if [[ $EUID -ne 0 ]]; then
 	echo "This script should be run as root to enable cache clearing" 1>&2
 	echo
 fi
 
+actions="input output"
+impls="1 2 3 4"
+
 format="\"%C\",%U,%S,%E,%R,%F,%w,%I,%O";
 
-filen=$(getconf OPEN_MAX);
-if (( $filen > 30 )) ; then
-	filen=30;
+max_open_files=$(getconf OPEN_MAX);
+if (( $max_open_files > 30 )) ; then
+	max_open_files=30;
 fi
 
-filelen() {
+file_counts="1 2 5 15 30"
+
+file_len() {
 	if [[ $1 == "1" ]]; then
 		echo 2097152; # 8 MiB
 	else
@@ -34,84 +37,118 @@ params() {
 	if [[ $1 == "3" ]]; then
 		echo "32 64 128 256 512 1024 4096 16384"
 	else
-		echo "1 4 16 64 256 1024 4096 16384"
+		if [[ $1 == "4" ]]; then
+			echo "1 4 16 64 256 1024 4096 8192"
+		else
+			echo "1"
+		fi
 	fi
 }
 
+clear_cache() {
 if [[ $EUID -eq 0 ]]; then
 	sync
 	echo 3 > /proc/sys/vm/drop_caches
 fi
+}
 
-echo
-echo Output
-echo
-
+clear_cache
 date --rfc-3339=seconds >> "$1"
 
-for impln in 1 2; do
-	len=$(filelen $impln)
-	echo ;
-	echo Implementation $impln;
-	for fn in $(seq 2 6 $filen); do
-		echo "$fn files"
-		if !  /usr/bin/time -a -o "$1" -f "$format" ./streams output $impln $fn $len ; then
-			exit 1 ;
-		fi
-	done
-done
-
-for impln in 3 4; do
-	len=$(filelen $impln)
-	echo ;
-	echo Implementation $impln;
-	for param in $(params $impln) ; do
-		for fn in $(seq 2 6 $filen); do
-			echo "$fn files"
-			if !  /usr/bin/time -a -o "$1" -f "$format" ./streams output $impln $fn $len $param ; then
-				exit 1 ;
-			fi
+for impl in $impls; do
+	echo "Implementation $impl";
+	for action in $actions; do
+		echo $action;
+		for param in $(params $impl); do
+			echo "Implementation $impl ($param)";
+			for file_count in $file_counts; do
+				if [ $file_count -le $max_open_files ] ; then
+					echo "$file_count files"
+					if [ $action = output ] ; then
+						if !  /usr/bin/time -a -o "$1" -f "$format" ./streams output $impl $file_count $(file_len $impl) $param ; then
+							exit 1 ;
+						fi
+					else
+						clear_cache
+						if !  /usr/bin/time -a -o "$1" -f "$format" ./streams output $impl $file_count $param ; then
+							exit 1 ;
+						fi
+					fi
+				fi
+			done
 		done
 	done
 done
 
-if [[ $EUID -eq 0 ]]; then
-	sync
-	echo 3 > /proc/sys/vm/drop_caches
-fi
-
-echo
-echo Input
-echo
-
-for impln in 1 2; do
-	echo ;
-	echo Implementation $impln;
-	for fn in $(seq 2 6 $filen); do
-		echo "$fn files"
-		if !  /usr/bin/time -a -o "$1" -f "$format" ./streams input $impln $fn ; then
-			exit 1 ;
-		fi
-		if [[ $EUID -eq 0 ]]; then
-			sync
-			echo 3 > /proc/sys/vm/drop_caches
-		fi
-	done
-done
-
-for impln in 3 4; do
-	echo ;
-	echo Implementation $impln;
-	for param in $(params $impln) ; do
-		for fn in $(seq 2 6 $filen); do
-			echo "$fn files"
-			if !  /usr/bin/time -a -o "$1" -f "$format" ./streams input $impln $fn $param ; then
-				exit 1 ;
-			fi
-			if [[ $EUID -eq 0 ]]; then
-				sync
-				echo 3 > /proc/sys/vm/drop_caches
-			fi
-		done
-	done
-done
+#
+#echo
+#echo Output
+#echo
+#
+#
+#for impln in 1 2; do
+#	len=$(file_len $impln)
+#	echo ;
+#	echo Implementation $impln;
+#	for fn in $(seq 2 6 $max_open_files); do
+#		echo "$fn files"
+#		if !  /usr/bin/time -a -o "$1" -f "$format" ./streams output $impln $fn $len ; then
+#			exit 1 ;
+#		fi
+#	done
+#done
+#
+#for impln in 3 4; do
+#	len=$(file_len $impln)
+#	echo ;
+#	echo Implementation $impln;
+#	for param in $(params $impln) ; do
+#		for fn in $(seq 2 6 $max_open_files); do
+#			echo "$fn files"
+#			if !  /usr/bin/time -a -o "$1" -f "$format" ./streams output $impln $fn $len $param ; then
+#				exit 1 ;
+#			fi
+#		done
+#	done
+#done
+#
+#if [[ $EUID -eq 0 ]]; then
+#	sync
+#	echo 3 > /proc/sys/vm/drop_caches
+#fi
+#
+#echo
+#echo Input
+#echo
+#
+#for impln in 1 2; do
+#	echo ;
+#	echo Implementation $impln;
+#	for fn in $(seq 2 6 $max_open_files); do
+#		echo "$fn files"
+#		if !  /usr/bin/time -a -o "$1" -f "$format" ./streams input $impln $fn ; then
+#			exit 1 ;
+#		fi
+#		if [[ $EUID -eq 0 ]]; then
+#			sync
+#			echo 3 > /proc/sys/vm/drop_caches
+#		fi
+#	done
+#done
+#
+#for impln in 3 4; do
+#	echo ;
+#	echo Implementation $impln;
+#	for param in $(params $impln) ; do
+#		for fn in $(seq 2 6 $max_open_files); do
+#			echo "$fn files"
+#			if !  /usr/bin/time -a -o "$1" -f "$format" ./streams input $impln $fn $param ; then
+#				exit 1 ;
+#			fi
+#			if [[ $EUID -eq 0 ]]; then
+#				sync
+#				echo 3 > /proc/sys/vm/drop_caches
+#			fi
+#		done
+#	done
+#done
