239d0585c705e719c4454c707f37959b4c5396b5
LordRaina <trinhjacky@yahoo.fr>
2018-01-04 14:03:13 +0100

    benchmark merge

--- src/benchmark-mergesort.sh
+++ src/benchmark-mergesort.sh
@@ -11,12 +11,12 @@ fi
 
 #date --rfc-3339=seconds >> "$1"
 
-for M in $(seq 131072 131072 4194304) ; do
+for M in 262144 524288 1048576 2097152 4194304; do
 	for d in 2 4 8 16 ; do
 		clear_cache
-		echo ./mergesort \"output data/od.0\" $M $d
+		echo ./mergesort \"output data/od.18\" $M $d
 		#if !  /usr/bin/time -a -o "$1" -f "$format" ./mergesort "output data/od.0" $M $d ; then
-		if !  /usr/bin/time ./mergesort "output data/od.0" $M $d ; then
+		if !  /usr/bin/time -p ./mergesort "output data/od.0" $M $d ; then
 			exit 1 ;
 		fi
 	done
